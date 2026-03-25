0d4d384e6cfc1837665ccbbb307691cfbae51910
NA <rushikeshgaikwad2565@gmail.com>
2026-03-25 08:31:56 +0530

    initial commite

---
+++ switch/index.js
@@ -0,0 +1,11 @@
+let day=3;
+switch(day){
+    case 1:
+        console.log("mon")
+        break;
+        case 2:
+            console.log("tue")
+            break;
+            default:
+              console.log("invalid choise")  
+}
---
+++ templeteliterals/index.js
@@ -0,0 +1,9 @@
+// let Name="Rushi";
+// let Income = "100000"
+// let PinCode=422606
+// console.log("your name is "+'"'+Name+'"'+ " income is "+Income+" pincode is "+PinCode)
+let Name="Rushi";
+let Income = "100000"
+let PinCode=422606
+//your name is "Rushi " income is 100000 pincode is 422606
+console.log(`your name is '${Name}' your income is ${Income} your pincode is ${PinCode}`)
